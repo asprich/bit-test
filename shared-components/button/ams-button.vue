ba1b6241d29e54271a05616ae81efcc2a3303967
Adam Sprich <asprich@cafepress.com>
2021-03-25 11:57:35 -0400

    button styling

--- shared-components/button/ams-button.vue
+++ shared-components/button/ams-button.vue
@@ -2,6 +2,19 @@
     <button @click="$emit('dostuff')" v-text="label"></button>
 </template>
 
+<style scoped>
+button {
+    display: inline-block;
+    background: #317de0;
+    color: #fff;
+    border: 1px solid #bbb;
+    border-radius: 4px;
+    padding: 10px 20px;
+    margin: 4px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
+
 <script>
 export default {
     name: "AmsButton",
